65a12eb8feccda1388f825cfd87d2669c91d820a
Alan Hortz <alan.hortz@gmail.com>
2014-02-02 17:49:10 +0100

    Creating the specs for the cassette player ...

---
+++ tests/spec/CassettePlayerSpec.js
@@ -0,0 +1,18 @@
+describe("A cassette player", function () {
+  describe("when created", function () {
+    it("is not powered up", function () {
+
+    });
+  });
+  describe("when powered up the first time", function () {
+    it("is not playing, it is stopped", function () {
+
+    });
+  });
+
+  describe("when powered up after has been powered off", function () {
+    it("should remember in which state it was, stopped or playing", function () {
+
+    });
+  });
+});
